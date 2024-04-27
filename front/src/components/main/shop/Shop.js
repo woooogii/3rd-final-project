94b0caa07efae41a2ae9bc36566da329d7947daf
woooogii <wldms9311@naver.com>
2024-04-27 17:41:03 +0900

    427지은

--- front/src/components/main/shop/Shop.js
+++ front/src/components/main/shop/Shop.js
@@ -43,18 +43,6 @@ const Shop = () => {
         fetchData();
     }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://localhost:4000/pedal/shop');
-                setEntities(response.data);
-            } catch (error) {
-                console.error('error_fetch', error);   
-            }
-        };
-        fetchData();
-    }, []);
-
     return (
         <>
             <div ref={startHereRef}>
