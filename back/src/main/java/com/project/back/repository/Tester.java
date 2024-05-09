3843ca2135689eefc04a2e3a36c7249c87c6b851
woooogii <155815722+woooogii@users.noreply.github.com>
2024-05-09 17:22:24 +0900

    Delete back/src/main/java/com/project/back/repository/Tester.java

--- back/src/main/java/com/project/back/repository/Tester.java
+++
@@ -1,9 +0,0 @@
-package com.project.back.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.back.entity.Test;
-
-public interface Tester extends JpaRepository<Test,Long>{
-
-}
